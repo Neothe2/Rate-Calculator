69bac0e0b4de34abd6ec3eab732ab786e7291f8f
NeoThe2 <neo.j.jabin@gmail.com>
2022-05-20 09:28:02 +0530

    finished the auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+// import firebase from 'firebase/compat/app'
+// import 'firebase/compat/auth';
+import * as firebase from 'firebase/compat';
+
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { User } from './user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  user$: Observable<User>;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+  ) {
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap((user) => {
+        if (user) {
+          return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+        } else {
+          return of(null);
+        }
+      })
+    );
+  }
+
+  async googleSignin() {
+    const provider = new firebase.default.auth.GoogleAuthProvider();
+    const credential = await this.afAuth.signInWithPopup(provider);
+    return this.updateUserData(credential.user);
+  }
+
+  async signOut() {
+    await this.afAuth.signOut();
+    this.router.navigate(['/']);
+  }
+
+  async updateUserData(user) {
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(
+      `users/${user.uid}`
+    );
+
+    const data = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoUrl: user.photoUrl,
+    };
+
+    return userRef.set(data, { merge: true });
+  }
+}
