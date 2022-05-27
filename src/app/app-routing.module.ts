9bbbe6142fd777fdb61a1cbe526e8c8e1d5c4034
NeoThe2 <neo.j.jabin@gmail.com>
2022-05-27 19:39:47 +0530

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { CalcPageComponent } from './calc-page/calc-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', component: AdminPageComponent },
+  { path: '', component: CalcPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
