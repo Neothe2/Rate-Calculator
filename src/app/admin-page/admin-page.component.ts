fc89302ad2d5de9756d3e970187ac7e1ee4b35e5
NeoThe2 <neo.j.jabin@gmail.com>
2022-05-30 09:32:44 +0530

    added the admin page component

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from '../core/auth.service';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
-  styleUrls: ['./admin-page.component.css']
+  styleUrls: ['./admin-page.component.css'],
 })
 export class AdminPageComponent implements OnInit {
+  user$: Observable<User>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(auth: AuthService) {
+    this.user$ = auth.user$;
   }
 
+  ngOnInit(): void {}
 }
